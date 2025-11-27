6e184b4b75a81fcaf3f4a33ca78b997915b5373e
omid.sed <osedighimornani@gmail.com>
2025-11-27 17:05:45 +0100

    refactoring 2a/2b: prepare_stan_data in helpers module

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,5 +1,5 @@
 from .model_data import ModelData
 from .model import Model
-from .helper_functions import comp_entropy
+from .helper_functions import comp_entropy, prepare_stan_data
 
-__all__ = ["ModelData", "Model", "comp_entropy"]
+__all__ = ["ModelData", "Model", "comp_entropy", "prepare_stan_data"]
--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import entropy
+from .model_data import ModelData
 
 
 # calculate entropy of a given sample
@@ -8,3 +9,27 @@ def comp_entropy(x):
     _, counts = np.unique(x, return_counts=True)
     probabilities = counts / len(x)
     return entropy(probabilities, base=np.e)  # natural log (ln)
+
+
+def prepare_stan_data(model_data: ModelData, S: int):
+    """
+    Bereitet die Daten für Stan vor.
+
+    CmdStanPy braucht die Daten als JSON-kompatibles Dictionary.
+    """
+    stan_data = {
+        "S": S,
+        "N_total": int(model_data.n_total),
+        "N_train": int(model_data.n_train),
+        "N_obs": int(model_data.n_obs),
+        "nCovs": int(model_data.n_covs),
+        "Time": [int(x) for x in model_data.time],
+        "Closed": [int(x) for x in model_data.closed],
+        "Days": [float(x) for x in model_data.days],
+        "Ratings": [int(x) for x in model_data.ratings],
+        "Sentiment": [float(x) for x in model_data.sentiment],
+        "Q": model_data.Q.tolist(),
+        "R": model_data.R.tolist(),
+        "X_test": model_data.X_test.tolist(),
+    }
+    return stan_data
