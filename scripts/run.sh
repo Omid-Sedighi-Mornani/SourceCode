148cca68f0e6253b7d4f5dbfa2c33339e07d4618
omid.sed <osedighimornani@gmail.com>
2025-12-04 01:19:08 +0100

    uv environment and server script set upped

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=training
+#SBATCH --partition=main
+#SBATCH --time=8:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+
+# Activate your environment and run
+cd ~/SourceCode
+uv run python scripts/main.py 
