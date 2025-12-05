1bbd1486451978b5dea327c5f14e404ccd41547c
omid.sed <osedighimornani@gmail.com>
2025-12-05 12:05:55 +0100

    comprehensive metrics added at the end

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,5 +1,10 @@
 from .model_data import ModelData
 from .model import Model
 from .helper_functions import comp_entropy, prepare_stan_data
+from .benchmark_helpers import (
+    get_model_performance,
+    create_model_comparison_df,
+    print_model_summary,
+)
 
 __all__ = ["ModelData", "Model", "comp_entropy", "prepare_stan_data"]
